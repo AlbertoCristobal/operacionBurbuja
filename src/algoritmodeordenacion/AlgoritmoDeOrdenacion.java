70ff1adef8b07f54501ce48ca1805be1db66c940
xp <xp@CETYSDAM028>
2019-04-25 13:07:15 +0200

    crreacion del insert

--- src/algoritmodeordenacion/AlgoritmoDeOrdenacion.java
+++ src/algoritmodeordenacion/AlgoritmoDeOrdenacion.java
@@ -11,7 +11,7 @@ package algoritmodeordenacion;
  */
 public class AlgoritmoDeOrdenacion {
 
-        int[] celdas ={11,17,21,55};
+        
 
     /**
      * @param args the command line arguments
@@ -19,24 +19,45 @@ public class AlgoritmoDeOrdenacion {
     public static void main(String[] args) {
         // TODO code application logic here
     
-     
+     int[] celdas = {64,17,21,55};
     
-       ordenacionBurbuja ejercicios = new ordenacionBurbuja();
-       System.out.println(Arrays.toString(ordenacionBurbuja.celdas));
-
+       AlgoritmoDeOrdenacion ejercicios = new AlgoritmoDeOrdenacion();
+       ejercicios.insercion(celdas);
+       for(int i=0 ;i<celdas.length;i++){
+           
+           System.out.println(celdas[i]);
+       }
+       
     }
-       public void ordenacionBurbuja( int []celdas ){
+    
+       public void ordenacionBurbuja( int [ ] celdas ){
             int aux;
-                for (int j=1; j<celdas.length; j++){
-                 for(int i=0; i < celdas.length -j; i++ ){
-                     
-                 if ( celdas[ i ] > celdas[i+1] ) {
-                 aux = celdas[ i ];
-                 celdas[ i ] = celdas[ i+1 ];
-                 celdas[ i+1 ] = aux;
-                     }
-                 }
-            }
-        } 
+             for (int j=1; j<celdas.length; j++){
+                for(int i=0; i < celdas.length -j; i++ ){
+                    if ( celdas[ i ] > celdas[i+1] ) {//intercambia los elementos
+                      aux = celdas[ i ];
+                     celdas[ i ] = celdas[ i+1 ];
+                    celdas[ i+1 ] = aux;
+                    }
+                }
+             }
+        }
+       
+      //EJERCICIO2
+       
+       public void insercion (int[]celdas){
+           for (int i=1;i<celdas.length;i++){
+               int auxiliar =  celdas[i];
+               
+               for(int j=0;j<i;j++){
+                   if(celdas[i]<=celdas[j])
+                      
+                       celdas[j+1] = celdas[j];
+                    }  
+               celdas[i]= auxiliar;
+               }
+           }
+           
+       
     
 }
